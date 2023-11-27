17137a913ec7c70a84072ddbd7f4812bf494c589
Chris-code240 <duah.marfochristian@gmail.com>
2023-11-27 22:21:25 +0000

    path reconfigured

--- app/urls.py
+++ app/urls.py
@@ -1,23 +1,24 @@
-from django.urls import re_path
+from django.urls import path, path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns = [
-    re_path('home/',views.home),
-    re_path('auth/',views.login),
-    re_path('login/user/',views.login_user),
-    re_path('register/user/',views.register_user),
-    re_path('delete/',views.delete_user),
-    re_path('verify-token/',views.verify_token),
-    re_path('verified/login/',views.authorized_login),
-    re_path('add-post/',views.addPost),
-    re_path('post/image/',views.render_image),
-    re_path('delete-post/',views.delete_post),
-    re_path('addpost/',views.render_addPost),
-    re_path('comment/',views.post_comment),
-    re_path('refresh-comments/',views.refresh_comment),
-    re_path('posts/search',views.search),
-    re_path('post/get/',views.home)
+    path('',views.home),
+    path('home/',views.home),
+    path('auth/',views.login),
+    path('login/user/',views.login_user),
+    path('register/user/',views.register_user),
+    path('delete/',views.delete_user),
+    path('verify-token/',views.verify_token),
+    path('verified/login/',views.authorized_login),
+    path('add-post/',views.addPost),
+    path('post/image/',views.render_image),
+    path('delete-post/',views.delete_post),
+    path('addpost/',views.render_addPost),
+    path('comment/',views.post_comment),
+    path('refresh-comments/',views.refresh_comment),
+    path('posts/search',views.search),
+    path('post/get/',views.home)
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
